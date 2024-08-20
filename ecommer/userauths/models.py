d1d21c47ed3fb193b2eb5f658fab5c72829c84ac
aron15951 <aronchoque72@gmail.com>
2024-08-20 14:28:41 -0500

    feat: config user admin and custom admin userauth

--- ecommer/userauths/models.py
+++ ecommer/userauths/models.py
@@ -3,10 +3,11 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     email = models. EmailField(unique=True)
     username = models.CharField(max_length=100)
+    bio = models.CharField(max_length=100)
 
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['usernase']
+    REQUIRED_FIELDS = ['username']
 
     def __str__(self):
         return self.username
